83c27102fa3a6cec39e67fffd92f669c9407641b
Larisa Kasyanova <lKasyanova@velox.biz>
2021-09-10 19:34:08 +0500

    input url

---
+++ chernovic/chernovic.js
@@ -0,0 +1,16 @@
+let b = -8;
+let a = 1;
+let c = 12;
+let z = b ** 2 - 4 * a * c;
+let x1 = (-b - 4) / 2 * a;
+let x2 = (b - 4) / 2 * a;
+console.log("answer discromenant:", z);
+console.log("answer x1:", x1);
+console.log("answer x2:", x2);
+
+let d = 25;
+let user = 26;
+let result = d >= user;
+console.log(result);
+
+
